ec276202e0f6ce30d424d8bb1285f17e5d32a706
Huan Jiang <bill.jiangh@gmail.com>
2024-05-15 15:57:23 +0800

    wezterm: window size and init position

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -12,9 +12,14 @@ config.macos_window_background_blur = 20
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+config.initial_cols = 240
+config.initial_rows = 60
+
 wezterm.on("gui-startup", function()
-	local tab, pane, window = wezterm.mux.spawn_window({})
-	window:gui_window():maximize()
+	local tab, pane, window = wezterm.mux.spawn_window({
+		position={x=0,y=200}
+	})
+	-- window:gui_window():maximize()
 end)
 
 return config
