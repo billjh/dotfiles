6c48e77d30ffb9119e322024cc09c24d23840f4d
Huan Jiang <bill.jiangh@gmail.com>
2024-08-02 15:11:51 +0800

    pane navigation keys

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -9,15 +9,53 @@ config.font_size = 15
 config.window_background_opacity = 1.0
 config.macos_window_background_blur = 20
 
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
 config.initial_cols = 240
 config.initial_rows = 60
 
+config.keys = {
+	{
+		key = 'w',
+		mods = 'SUPER',
+		action = wezterm.action.CloseCurrentPane { confirm = false },
+	},
+	{
+		key = 'e',
+		mods = 'SUPER',
+		action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+	},
+	{
+		key = 'e',
+		mods = 'SUPER|SHIFT',
+		action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+	},
+	{
+		key = 'LeftArrow',
+		mods = 'SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Left',
+	},
+	{
+		key = 'RightArrow',
+		mods = 'SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Right',
+	},
+	{
+		key = 'UpArrow',
+		mods = 'SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Up',
+	},
+	{
+		key = 'DownArrow',
+		mods = 'SUPER',
+		action = wezterm.action.ActivatePaneDirection 'Down',
+	},
+}
+
 wezterm.on("gui-startup", function()
 	local tab, pane, window = wezterm.mux.spawn_window({
-		position={x=0,y=200}
+		position = { x = 0, y = 200 }
 	})
 	-- window:gui_window():maximize()
 end)
