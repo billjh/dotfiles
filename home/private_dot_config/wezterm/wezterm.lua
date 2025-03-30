cb4386d02c464e1e9408f5e37354ef2914dbc986
Huan Jiang <bill.jiangh@gmail.com>
2025-03-30 15:09:15 +0800

    wezterm updates

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -10,8 +10,9 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 
 -- config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 -- config.enable_scroll_bar = true
+config.use_fancy_tab_bar = false
 config.scrollback_lines = 100000
 
 -- config.initial_cols = 80
